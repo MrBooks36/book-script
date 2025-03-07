d5d7fe0f8f4d4a4e24657a9ee740e8aa0911572d
MrBooks36 <mrbooks36@outlook.com>
2025-03-08 07:47:49 +1000

    stuff

---
+++ run.py
@@ -0,0 +1,3 @@
+from os import system 
+import sys
+system(f'runner/venv/Scripts/python.exe {int(sys.argv)[1]}')
---
+++ runner/run.py
@@ -0,0 +1,3 @@
+from os import system 
+import sys
+system(f'python.exe {int(sys.argv)[1]}')
